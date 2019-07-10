2382578b4b091c83f4a916f2b54c9b71decccbd7
JC <jc@test.fr>
2019-07-10 14:22:10 +0200

    ajout du print salut les codeurs

---
+++ SourceControl/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SourceControl
+//
+//  Created by Jc mac on 10/07/2019.
+//  Copyright © 2019 Jean Christophe Boussiquet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Salut les codeurs")
+    }
+
+
+}
+
